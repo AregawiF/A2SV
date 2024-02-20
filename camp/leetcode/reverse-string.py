32abe6a356de0ae5be89fa5417efb4853cccefc3
AregawiF <133565354+AregawiF@users.noreply.github.com>
2024-02-20 21:20:46 +0300

    Add solution for Reverse String

--- camp/leetcode/reverse-string.py
+++ camp/leetcode/reverse-string.py
@@ -1,10 +1,17 @@
-class Solution(object):
-    def reverseString(self, s):
-        """
-        :type s: List[str]
-        :rtype: None Do not return anything, modify s in-place instead.
-        """
-        for i in range(len(s)//2):
-            s[i],s[(len(s) - 1 -i)] = s[(len(s) - 1 -i)],s[i]
-
-        
+class Solution:
+    def reverseString(self, s: List[str], c =0, right = 0, left = 0) -> None:
+        """        Do not return anything, modify s in-place instead.   """
+        if len(s) == 1 or len(s) == 0:
+            return
+        # elif len(s) == 2:
+        #     s[0], s[1] = s[1], s[0]
+        if c == 0:    
+            left = 0
+            right = len(s)-1
+        elif c == (len(s) // 2):
+            return
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+        c += 1
+        self.reverseString(s,c, right, left)
